f3fded43cda9cda1c3ba3de275f9046b3bc74c6b
Odigene Joseph <Odigene12@gmail.com>
2016-11-14 20:24:35 -0600

    tried to solve problem of username

---
+++ Tweeter/Scripts/app.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module("tweet", []);
+
+app.controller("RegisterCtrl", [$scope, $http], function ($scope, $http){
+
+    var checkUsername = function (username, method){
+
+        return $http({
+            url: "/api/TwitUsername/5",
+            method: "GET"
+        })
+    } 
+
+})
